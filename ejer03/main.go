8709e2e9db35829fceb00a5672d257668cd1ceab
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-06-11 11:35:15 -0500

    Condicional Switch

--- ejer03/main.go
+++ ejer03/main.go
@@ -5,10 +5,18 @@ import "fmt"
 var estado bool
 
 func main() {
-	estado = true
-	if estado = false; estado == true {
-		fmt.Println(estado)
-	} else if estado == false {
-		fmt.Println("Es Falso")
+	switch numero := 7; numero {
+	case 1:
+		fmt.Println(1)
+	case 2:
+		fmt.Println(2)
+	case 3:
+		fmt.Println(3)
+	case 4:
+		fmt.Println(4)
+	case 5:
+		fmt.Println(5)
+	default:
+		fmt.Println("Mayor a 5")
 	}
 }
