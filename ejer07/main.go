37a612e9ecfad2c86f34f0d2ecf6a2054005a2e8
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-06-13 11:30:19 -0500

    Funciones Anónimas

---
+++ ejer07/main.go
@@ -0,0 +1,46 @@
+package main
+
+import (
+	"fmt"
+)
+
+// Calculo : Variable que contiene función anonima.
+var Calculo func(int, int) int = func(num1 int, num2 int) int {
+	return num1 + num2
+}
+
+func main() {
+	fmt.Printf("Suma 5 + 8 = %d \n", Calculo(5, 8))
+
+	// Restamos
+	Calculo = func(num1 int, num2 int) int {
+		return num1 - num2
+	}
+	fmt.Printf("Restamos 6 - 4 = %d \n", Calculo(6, 4))
+
+	// Dividimos
+	Calculo = func(num1 int, num2 int) int {
+		return num1 / num2
+	}
+	fmt.Printf("Dividimos 24 / 8 = %d \n", Calculo(24, 8))
+
+	// Multiplicamos
+	Calculo = func(num1 int, num2 int) int {
+		return num1 * num2
+	}
+	fmt.Printf("Multiplicamos 11 * 6 = %d \n", Calculo(11, 6))
+
+	Operaciones()
+
+}
+
+// Operaciones : Otra manera de crear una función anónima
+func Operaciones() {
+	resultado := func() int {
+		var a int = 23
+		var b int = 13
+		return a + b
+	}
+	fmt.Println(resultado())
+
+}
