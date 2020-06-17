7bfa5ee26bad1e96dd4d7fb3df64b1f6834a823a
Marco Ruvalcaba <desarrollo@oversistemas.com>
2020-06-17 11:16:53 -0500

    Slice

--- ejer08/main.go
+++ ejer08/main.go
@@ -2,11 +2,34 @@ package main
 
 import "fmt"
 
-var matriz [5][7]int
-
 func main() {
-	matriz[3][5] = 1
+	// matriz := []int{2, 5, 4}
 
-	fmt.Println(matriz)
+	// fmt.Println(matriz)
+
+	variante2()
+	variante3()
+	variante4()
+}
+
+func variante2() {
+	elementos := [5]int{1, 2, 3, 4, 5}
+	//la porcion corresponde desde el vector 3 a la ultima
+	porcion := elementos[3:]
+	fmt.Println(porcion)
+
+}
+
+func variante3() {
+	elementos := make([]int, 5, 20)
+	fmt.Printf("Largo %d, Capacidad %d", len(elementos), cap(elementos))
+}
+
+func variante4() {
+	nums := make([]int, 0, 0)
+	for i := 0; i < 100; i++ {
+		nums = append(nums, i)
+	}
+	fmt.Printf("\n Largo %d, Capacidad %d", len(nums), cap(nums))
 
 }
